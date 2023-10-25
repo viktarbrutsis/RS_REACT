9ee3590959e91bc839c3d74cf5ac71f5166f147c
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2023-10-25 09:15:09 +0300

    feat: correct lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,5 +31,6 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };

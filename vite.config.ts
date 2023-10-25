5546fab4b45cc5336e57ac7e549dedc8392620fa
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2023-10-25 13:32:26 +0300

    feat: add husky

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vitePluginSass from 'vite-plugin-sass';
 
-
-import vitePluginSass from 'vite-plugin-sass';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginSass()],
